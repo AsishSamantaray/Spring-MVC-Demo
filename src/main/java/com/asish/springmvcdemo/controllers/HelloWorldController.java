9c82beced304a15c0933d04ecc57d70db3019058
AsishSamantaray <samantaray.asish04@gmail.com>
2020-03-30 23:06:14 +0530

    Simple Controller and view.

--- src/main/java/com/asish/springmvcdemo/controllers/HelloWorldController.java
+++ src/main/java/com/asish/springmvcdemo/controllers/HelloWorldController.java
@@ -1,11 +1,8 @@
 package com.asish.springmvcdemo.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
