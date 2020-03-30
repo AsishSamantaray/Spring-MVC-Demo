48dad531a0d7ec4436dd384aab587dc5e6d76d8c
AsishSamantaray <samantaray.asish04@gmail.com>
2020-03-30 11:57:42 +0530

    Spring MVC - Creating Form Handling in simple / basic way.

---
+++ src/main/java/com/asish/springmvcdemo/controllers/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.asish.springmvcdemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping(value = "/showForm", method = RequestMethod.GET)
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    // Basic / Simple mapping in Spring MVC.
+    @RequestMapping(value = "/processForm", method = RequestMethod.POST)
+    public String showResponse() {
+        return "helloworld";
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/views/helloworld-form.jsp
@@ -0,0 +1,16 @@
+<%--
+  User: Asish Samantaray
+  Date: 30-03-2020
+  Time: 00:41
+--%>
+<html>
+    <head>
+        <title>Form Demo</title>
+    </head>
+    <body>
+        <form action="processForm" method="post">
+            <input type="text" name="name" placeholder="What's your name?"><br>
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
